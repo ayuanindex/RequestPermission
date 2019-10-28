07c7526accc142247ca357f9d6ad9d58e08b4fa9
AYuan <lzy2118053626@hotmaill.com>
2019-10-29 00:07:35 +0800

    update

--- app/src/main/java/com/ayuan/permission/api/PermissionManager.java
+++ app/src/main/java/com/ayuan/permission/api/PermissionManager.java
@@ -2,7 +2,6 @@ package com.ayuan.permission.api;
 
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 
 import com.ayuan.permission.BaseActivity;
@@ -11,31 +10,10 @@ import java.util.ArrayList;
 
 public class PermissionManager {
     public BaseActivity baseActivity;
-    private String[] unauthorizedPermission;
-
     /**
-     * 检查已经申请的权限,将为申请的权限保存起来
-     *
-     * @param permissions
-     * @return true 表示所有权限已经申请 false 表示部分权限被拒绝
+     * 不被允许的权限集合
      */
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    public boolean haveBoolean(String... permissions) {
-        ArrayList<String> permissionList = new ArrayList<>();
-        for (String permission : permissions) {
-            // 检查需要申请的权限在此应用中是否存在
-            if (baseActivity.checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
-                permissionList.add(permission);
-            }
-        }
-
-        // 将List转换成array数组
-        if (permissionList.size() > 0) {
-            this.unauthorizedPermission = permissionList.toArray(new String[permissionList.size()]);
-            return false;
-        }
-        return true;
-    }
+    private String[] unauthorizedPermission;
 
     /**
      * 请求获取权限
@@ -45,13 +23,42 @@ public class PermissionManager {
     public void requestPermission(BaseActivity baseActivity) {
         this.baseActivity = baseActivity;
         RequestPermission permission = baseActivity.getClass().getAnnotation(RequestPermission.class);
+        //如果permission为空，则没有权限申请
         if (permission == null) {
             return;
         }
 
+        //如果当前Android版本>=Android6.0版本并且有未被允许的权限则重新申请（haveBoolean方法有无不被允许的权限）
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !haveBoolean(permission.value())) {
             baseActivity.requestPermissions(unauthorizedPermission, 1);
         }
+    }
+
+    /**
+     * 检查已经申请的权限,将为申请的权限保存起来
+     *
+     * @param permissions
+     * @return true 表示所有权限已经申请 false 表示部分权限被拒绝
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public boolean haveBoolean(String... permissions) {
+        //没有被允许的权限集合
+        ArrayList<String> permissionList = new ArrayList<>();
+        //循环检查每个权限是否申请
+        for (String permission : permissions) {
+            // 检查需要申请的权限在此应用中是否存在
+            if (baseActivity.checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                //如果权限没有被允许，则添加到没有被允许的权限集合中
+                permissionList.add(permission);
+            }
+        }
+
+        // 如果存在没有被允许的权限则将List转换成array数组
+        if (permissionList.size() > 0) {
+            this.unauthorizedPermission = permissionList.toArray(new String[permissionList.size()]);
+            return false;
+        }
+        return true;
     }
 
     /**
